5ef4ff55f07ed3b6b0c5bf981d1ba7309c242b1a
jminie <jminie@naver.com>
2021-06-07 21:07:34 +0900

    JAVA_method_3

---
+++ AccountingApp.java
@@ -0,0 +1,27 @@
+
+public class AccountingApp {
+	
+	public static double valueOfSupply = 10000.0;
+	
+	public static double vatRate = 0.1;
+			
+	public static double getVAT() {
+		return valueOfSupply * vatRate;
+	}
+	public static double getTotal() {
+		return valueOfSupply + getVAT();
+	}
+
+	public static void main(String[] args) {
+		
+		double vat = getVAT();
+		
+		double total = getTotal();
+		
+		System.out.println("Value of supply : " +valueOfSupply);
+		System.out.println("VAT : " +getVAT());
+		System.out.println("Total : " +getTotal());
+
+	}
+
+}
